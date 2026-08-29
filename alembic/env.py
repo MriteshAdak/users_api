78034559d5b611bc4cc9bbc8fd14beba904bd8f2
MriteshAdak <mriteshadak@outlook.com>
2026-08-29 18:22:59 +0200

    Set Alembic configuration options for database URL and schema

--- alembic/env.py
+++ alembic/env.py
@@ -10,6 +10,9 @@ from src.users_api.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+settings = get_settings()
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL) #type: ignore
+config.set_main_option("database.schema", settings.USER_API_DB_SCHEMA) #type: ignore
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
